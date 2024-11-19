f2a46b37851baabb1ae22ecc80c2f2cda5b74576
Sarikhin <37961417+IKHINtech@users.noreply.github.com>
2024-11-19 15:56:29 +0700

    Update treesiter-context.lua

--- nvim/lua/plugins/treesiter-context.lua
+++ nvim/lua/plugins/treesiter-context.lua
@@ -3,8 +3,7 @@ return {
   event = "VeryLazy",
   opts = function()
     local tsc = require("treesitter-context")
-
-    LazyVim.toggle.map("<leader>ut", {
+    Snacks.toggle({
       name = "Treesitter Context",
       get = tsc.enabled,
       set = function(state)
@@ -14,8 +13,7 @@ return {
           tsc.disable()
         end
       end,
-    })
-
+    }):map("<leader>ut")
     return { mode = "cursor", max_lines = 3 }
   end,
 }
